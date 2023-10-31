a87c9a42887c1a06fa7541cc997fe7f16f586566
Anna Ulanova <anetteua0@gmail.com>
2023-10-31 15:01:21 -0700

    initial layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const screenWidth = Dimensions.get('screen').width;
+const buttonWidth = screenWidth / 5;
+const margin = buttonWidth / 10;
+
+const buttons = [
+	'AC',
+	'+/-',
+	'%',
+	'/',
+	'7',
+	'8',
+	'9',
+	'x',
+	'4',
+	'5',
+	'6',
+	'-',
+	'1',
+	'2',
+	'3',
+	'+',
+	'0',
+	'.',
+	'='
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const renderItem = (item: String) => {
+		return (
+			<View
+				style={[
+					styles.buttonContainer,
+					{ width: item === '0' ? (buttonWidth + margin) * 2 : buttonWidth }
+				]}
+			>
+				<Text>{item}</Text>
+			</View>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<FlatList
+				data={buttons}
+				numColumns={4}
+				renderItem={({ item }) => renderItem(item)}
+				contentContainerStyle={styles.flatlistContainer}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		top: buttonWidth,
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	buttonContainer: {
+		height: buttonWidth,
+		borderRadius: 100,
+		backgroundColor: 'red',
+		margin: margin,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	flatlistContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });

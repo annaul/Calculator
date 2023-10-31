44318a9856c7dcecaa5733d604437d690c3d244d
Anna Ulanova <anetteua0@gmail.com>
2023-10-31 16:17:52 -0700

    add colors

--- App.tsx
+++ App.tsx
@@ -1,50 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions } from 'react-native';
+import { ColorValue, Dimensions, ViewStyle } from 'react-native';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 const screenWidth = Dimensions.get('screen').width;
-const buttonWidth = screenWidth / 5;
-const margin = buttonWidth / 10;
+const buttonWidth = screenWidth / 5.5;
+const margin = buttonWidth / 11;
 
-const buttons = [
-	'AC',
-	'+/-',
-	'%',
-	'/',
-	'7',
-	'8',
-	'9',
-	'x',
-	'4',
-	'5',
-	'6',
-	'-',
-	'1',
-	'2',
-	'3',
-	'+',
-	'0',
-	'.',
-	'='
+interface Button {
+	value: string;
+	color: ColorValue;
+	textColor: ColorValue;
+}
+
+const buttons: Button[] = [
+	{ value: 'AC', color: '#a5a5a5', textColor: '#050505' },
+	{ value: '+/-', color: '#a5a5a5', textColor: '#050505' },
+	{ value: '%', color: '#a5a5a5', textColor: '#050505' },
+	{ value: '/', color: '#ff9f06', textColor: '#ffffff' },
+	{ value: '7', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '8', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '9', color: '#333333', textColor: '#f6f6f6' },
+	{ value: 'x', color: '#ff9f06', textColor: '#ffffff' },
+	{ value: '4', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '5', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '6', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '-', color: '#ff9f06', textColor: '#ffffff' },
+	{ value: '1', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '2', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '3', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '+', color: '#ff9f06', textColor: '#ffffff' },
+	{ value: '0', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '.', color: '#333333', textColor: '#f6f6f6' },
+	{ value: '=', color: '#ff9f06', textColor: '#ffffff' }
 ];
 
 export default function App() {
-	const renderItem = (item: String) => {
+	const renderItem = (item: Button) => {
+		const width = item.value === '0' ? (buttonWidth + margin) * 2 : buttonWidth;
 		return (
 			<View
-				style={[
-					styles.buttonContainer,
-					{ width: item === '0' ? (buttonWidth + margin) * 2 : buttonWidth }
-				]}
+				style={[styles.buttonContainer, { width, backgroundColor: item.color }]}
 			>
-				<Text>{item}</Text>
+				<Text style={[styles.buttonText, { color: item.textColor }]}>
+					{item.value}
+				</Text>
 			</View>
 		);
 	};
 
 	return (
 		<View style={styles.container}>
-			<StatusBar style="auto" />
+			<StatusBar backgroundColor="#000000" />
 			<FlatList
 				data={buttons}
 				numColumns={4}
@@ -59,14 +65,13 @@ const styles = StyleSheet.create({
 	container: {
 		top: buttonWidth,
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#000000',
 		alignItems: 'center',
 		justifyContent: 'center'
 	},
 	buttonContainer: {
 		height: buttonWidth,
 		borderRadius: 100,
-		backgroundColor: 'red',
 		margin: margin,
 		alignItems: 'center',
 		justifyContent: 'center'
@@ -75,5 +80,9 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	buttonText: {
+		fontSize: 30,
+		fontWeight: '600'
 	}
 });

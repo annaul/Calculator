91b84b336ce5f9a95036fb800c3b40a1b6b88c82
Anna Ulanova <anetteua0@gmail.com>
2023-11-01 14:42:59 -0700

    break up functions

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { SetStateAction, useState } from 'react';
 import { FlatList, Text, View, TouchableOpacity } from 'react-native';
 import { getButtonWidth, styles } from './styles';
-import { buttons, specialCharacters } from './data';
+import { buttons } from './data';
 import { Button } from './types';
-import { append, calculate } from './calculationHelper';
+import { append, calculate, isDigitOrDecimal, isSpecialCharacter } from './calculationHelper';
 
 export default function App() {
 	const [prevValue, setPrevValue] = useState<string>('0');
@@ -13,39 +13,62 @@ export default function App() {
 	const [displayValue, setDisplayValue] = useState<string>('0');
 
 	const updateValue = (value: string) => {
-		if (value === 'AC') {
-			setPrevValue('0');
-			setCurrentValue('0');
-			setOperation('');
-			setDisplayValue('0');
-		} else if (specialCharacters.includes(value)) {
-			const display = calculate(operation, prevValue, currentValue) || '0';
-			if (operation !== '') {
-				setPrevValue(display);
-				setDisplayValue(display);
-			} else {
-				setPrevValue(currentValue);
-				setDisplayValue(currentValue);
-			}
-			setOperation(value);
-			setCurrentValue('0');
-		} else if (!specialCharacters.includes(value) && value !== '=') {
-			if (currentValue === '0') {
-				setCurrentValue(value);
-				setDisplayValue(value);
-			} else {
-        const result = append(currentValue, value)
-				setDisplayValue(result);
-				setCurrentValue(result);
-			}
-		} else if (value === '=') {
-			const display = calculate(operation, prevValue, currentValue) ||'0';
-			setDisplayValue(display);
-			setCurrentValue('0');
-			setPrevValue('0');
-			setOperation('');
-		}
-	};
+  // Check for the AC (All Clear) case
+  if (value === 'AC') {
+    resetCalculator();
+  }
+  // Check for special characters (+, -, *, /)
+  else if (isSpecialCharacter(value)) {
+    handleSpecialCharacter(value);
+  }
+  // Check for digits and decimal point
+  else if (isDigitOrDecimal(value)) {
+    handleDigitOrDecimal(value);
+  }
+  // Check for the equals sign (=)
+  else if (value === '=') {
+    calculateResult();
+  }
+};
+
+const resetCalculator = () => {
+  setPrevValue('0');
+  setCurrentValue('0');
+  setOperation('');
+  setDisplayValue('0');
+};
+
+const handleSpecialCharacter = (value: SetStateAction<string>) => {
+  const display = calculate(operation, prevValue, currentValue) || '0';
+  if (operation !== '') {
+    setPrevValue(display);
+    setDisplayValue(display);
+  } else {
+    setPrevValue(currentValue);
+    setDisplayValue(currentValue);
+  }
+  setOperation(value);
+  setCurrentValue('0');
+};
+
+const handleDigitOrDecimal = (value: string) => {
+  if (currentValue === '0') {
+    setCurrentValue(value);
+    setDisplayValue(value);
+  } else {
+    const result = append(currentValue, value);
+    setDisplayValue(result);
+    setCurrentValue(result);
+  }
+};
+
+const calculateResult = () => {
+  const display = calculate(operation, prevValue, currentValue) || '0';
+  setDisplayValue(display);
+  setCurrentValue('0');
+  setPrevValue('0');
+  setOperation('');
+};
 
 	const renderItem = (item: Button) => {
 		const width = getButtonWidth(item.value);
--- calculationHelper.ts
+++ calculationHelper.ts
@@ -1,3 +1,5 @@
+import { specialCharacters } from "./data";
+
 export const calculate = (char: string, prevValue: string, currentValue: string) => {
 	if (char === '/') {
 		return (parseFloat(prevValue) / parseFloat(currentValue)).toString();
@@ -13,3 +15,6 @@ export const calculate = (char: string, prevValue: string, currentValue: string)
 export const append = (a: string, b: string) => {
 	return a + '' + b
 }
+
+export const isSpecialCharacter = (value: string) => specialCharacters.includes(value);
+export const isDigitOrDecimal = (value: string) => !specialCharacters.includes(value) && value !== '=';
